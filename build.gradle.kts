1c5b9c9136e33b123eaa38057aef519ba7a774c5
e.aganin <e.aganin@tbank.ru>
2025-10-12 23:57:34 +0300

    added integration tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,10 +61,3 @@ tasks.withType<Test> {
         "--add-opens", "java.base/java.util=ALL-UNNAMED"
     )
 }
-
-dependencyManagement {
-    imports {
-        mavenBom("org.flywaydb:flyway-bom:11.9.1")
-    }
-}
-
